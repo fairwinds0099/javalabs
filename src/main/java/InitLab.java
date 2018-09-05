0254dc2ceecf8ae2c90f06da51723bc336e31e61
macikgozm <42034601+macikgozm@users.noreply.github.com>
2018-09-04 21:45:49 -0500

    Update InitLab.java

--- src/main/java/InitLab.java
+++ src/main/java/InitLab.java
@@ -1,7 +1,8 @@
 public class InitLab {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    for (int i =0; i < 10; i++)
+      System.out.println("Hello World");
   }
 
 }
